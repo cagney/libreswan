b976d98d33d6b3270bf710dc5bdfd7628730256f
Andrew Cagney <cagney@gnu.org>
2025-01-31 14:45:12 -0500

    testing x509: use NSS generated certs

--- testing/guestbin/keys.sh
+++ testing/guestbin/keys.sh
@@ -32,9 +32,16 @@ cp /testing/x509/strongswan-ec-gen.sh /tmp/x509
 :
 
 /testing/x509/generate.sh /tmp/x509
+
 { cd /tmp/x509 && ./dist_certs.py ; }
 { cd /tmp/x509 && ./strongswan-ec-gen.sh ; }
 /testing/x509/selfsigned.sh /tmp/x509/selfsigned
+
+for host in east west road north nic ; do
+    cp -v /tmp/x509/real/mainca/${host}.all.p12  /tmp/x509/pkcs12/mainca/${host}.p12
+    cp -v /tmp/x509/real/mainca/${host}.end.cert /tmp/x509/certs/${host}.crt
+    cp -v /tmp/x509/real/mainca/${host}.end.key  /tmp/x509/keys/${host}.key
+done
 
 :
 : copy the certs from guest to host in a tar ball to avoid 9fs bug
