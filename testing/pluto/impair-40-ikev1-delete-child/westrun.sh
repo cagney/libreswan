f55df461fb56cfd0179aa461f6794eb7a3a7aa2c
Andrew Cagney <cagney@gnu.org>
2026-09-03 16:34:03 -0400

    testing: update tests using --impair v1_{isakmp,ipsec}_delete_payload

--- testing/pluto/impair-40-ikev1-delete-child/westrun.sh
+++ testing/pluto/impair-40-ikev1-delete-child/westrun.sh
@@ -1,5 +1,5 @@
 ipsec auto --up west-east
 # send to delete child SAs
-ipsec whack --impair v1_isakmp_delete_payload:duplicate
-ipsec whack --impair v1_ipsec_delete_payload:duplicate
+ipsec whack --impair v1_isakmp_delete_payload:emit_duplicate
+ipsec whack --impair v1_ipsec_delete_payload:emit_duplicate
 ipsec auto --delete west-east
